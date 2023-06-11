588a83f4b7b04d6c26a32402f98a0618baf00beb
Divyansh Joshi <98253145+Divyansh00001@users.noreply.github.com>
2023-06-11 21:22:15 +0530

    Update main_Activity.java

--- main_Activity.java
+++ main_Activity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Ac tivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
